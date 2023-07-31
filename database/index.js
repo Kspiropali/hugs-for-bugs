ce4372c71a970fbc88fd3496c42572d6a2c191b7
kspiropali <kspiropali@hotmail.com>
2023-07-31 14:52:33 +0100

    login works with cookies/redirect

--- database/index.js
+++ database/index.js
@@ -1,3 +1,39 @@
-const asd = "asdasd";
+let students = require("./students");
+let questions = require("./questions");
 
-module.exports = { asd };
+const findStudentByName = (name) => {
+  let foundStudent = students.find((student) => student.name === name);
+  return foundStudent ? foundStudent : null;
+};
+
+const findStudentBySchool = (schoolName) => {
+  return students.find((student) => student.school === schoolName);
+};
+
+const findBestStudent = () => {
+  // TODO: decide on a stats object layout
+};
+
+const findQuestionsByTopic = (topic) => {
+  return questions.find((question) => question.topic === topic);
+};
+
+const findAnswerByQuestion = (question) => {
+  for (const topic of questions) {
+    const foundQuestion = topic.questions.find(
+      (question) => question.question === questionToFind
+    );
+    if (foundQuestion) {
+      return foundQuestion.answer;
+    }
+  }
+  return "No such question in the database!";
+};
+
+module.exports = {
+  findStudentByName,
+  findStudentBySchool,
+  findBestStudent,
+  findQuestionsByTopic,
+  findAnswerByQuestion,
+};
--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
-const path = require('path')
 
-
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
-const project_dir = path.join(__dirname, '..');
-
 const app = express();
+
+const project_dir = path.join(__dirname, "..");
+const dbUtils = require("../database/index");
 
 app.use(cors());
 app.use(express.json());
@@ -22,18 +22,31 @@ app.get("/", (req, res) => {
 
 // user login api via their name
 app.post("/login", (req, res) => {
-  
+  const name = req.body.name;
+
+  if (!name || name.length === 0) {
+    res.status(400).send("Name is needed!");
+    return;
+  }
+
+  let status = dbUtils.findStudentByName(name);
+  console.log(status);
+  if (!status) {
+    res.status(401).send("Student's name does not exist in the database!");
+    return;
+  }
+
+  res
+    .cookie("user", name, { maxAge: 900000, httpOnly: true })
+    .json({ status: "Success", redirect: "/" })
+    .send();
 });
 
 // random question api, TODO: fix question being generated 2 times in a row
-app.post("/question", (req, res) => {
-  
-});
+app.post("/question", (req, res) => {});
 
 // verify question api, TODO: should accept {username: username, question: question}
 // should possibly return {outcome: "false || true", answer: answer}
-app.post("/question/verify", (req, res) => {
-  
-});
+app.post("/question/verify", (req, res) => {});
 
 module.exports = app;
