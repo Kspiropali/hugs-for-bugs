d6c229b05da5505adfbfc7d1526347013cf220f3
Balram Singh <balram_singh08@hotmail.com>
2023-08-01 20:58:50 +0100

    ok

--- database/students.js
+++ database/students.js
@@ -1,7 +1,7 @@
 let students = [
-  { "name": "Test", "school": "LFA", "stats": [] },
-  { "name": "John", "school": "LFA" },
-  { "name": "Nick", "school": "LFA" }
+  { name: "Test", "school": "LFA", "stats": [] },
+  { name: "John", "school": "LFA" },
+  { name: "Nick", "school": "LFA" }
 ]
 
 module.exports = students;
